49ee3699ee440c98583522ba172aa63fb1e3280e
cmoonth <cmoonth47@gmail.com>
2023-12-24 13:39:27 +0900

    12 /23 MemberController, MemberForm

--- jpa/src/main/java/jpabook/jpa/controller/MemberForm.java
+++ jpa/src/main/java/jpabook/jpa/controller/MemberForm.java
@@ -1,2 +1,19 @@
-package jpabook.jpa.controller;public class MemberForm {
+package jpabook.jpa.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
 }
